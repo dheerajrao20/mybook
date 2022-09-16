a3198310f1ecf1f6ceba2a8951d0626f318e0dbe
DHEERAJ KUMAR <dheerajrao20@outlook.com>
2022-09-16 17:05:38 +0530

    vid 46

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -12,11 +12,14 @@ router.post('/',[
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
-    }User.create({
+    }
+    User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
-      }).then(user => res.json(user));
+      }).then(user => res.json(user))
+         .catch(err=>{console.log(err)  //catches error and thenn console it
+        res.json({error:'Please enter a unique value', message:err.message})})  // showing it on server
     
 })
 
