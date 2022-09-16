13e10891f69d3543078b846383d1398fcb04b86d
DHEERAJ KUMAR <dheerajrao20@outlook.com>
2022-09-16 18:31:11 +0530

    vid 48

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 const User = require('../models/Users.js')
 const { body, validationResult } = require('express-validator');
@@ -7,16 +8,20 @@ router.post('/createuser',[
     body('name').isLength({ min: 5 }),
     body('email').isEmail(),
     body('password').isLength({ min: 5 }),
-], (req, res)=>{
+], async (req, res)=>{
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(req.body.password, salt)
+
     User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       }).then(user => res.json(user))
          .catch(err=>{console.log(err)  //catches error and thenn console it
         res.json({error:'Please enter a unique value', message:err.message})})  // showing it on server
