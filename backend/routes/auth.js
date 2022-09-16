d6b75dff4b4ec8e54fcf17490a999259be2e605f
DHEERAJ KUMAR <dheerajrao20@outlook.com>
2022-09-16 13:17:28 +0530

    16 sept

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/Users.js')
+const { body, validationResult } = require('express-validator');
 
-router.get('/', (req, res)=>{
+router.post('/',[
+    body('name').isLength({ min: 5 }),
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 }),
+], (req, res)=>{
 
-    console.log(req.body);
-    res.send("hello world");
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      }).then(user => res.json(user));
+    
 })
 
 module.exports = router 
